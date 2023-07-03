70fb35465ec5d780d77c5e2099d48c3bceade103
AAZZAZRON <aaronzhu3.1415@gmail.com>
2023-07-03 09:21:49 -0400

    add flask for uptime

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import bot
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def handle_get_request():
+  return "Success"
+
 
 if __name__ == '__main__':
-    bot.run_discord_bot()
-
+  bot.run_discord_bot()
+  app.run(host='0.0.0.0', port=8000)
